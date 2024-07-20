e70c84cc4c5986277ffb77918710aeb4ae227904
adilzada <100288857+adilzada@users.noreply.github.com>
2024-07-20 14:51:56 +0400

    kndkwnf

--- Main.java
+++ Main.java
@@ -1,3 +1,16 @@
 public class Main {
 
+public static void main(String[] args){
+;
+    System.out.println(intMethod2(intMethod(10,15)));
+    System.out.println("jwdjhjwfwf");
+}
+public static int intMethod2(int x){
+    return x+5;
+}
+
+public static int intMethod(int a,int b){
+    int c=a+b;
+    return c;
+}
 }
